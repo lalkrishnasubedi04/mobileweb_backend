1eca29d5d6e0dafb1acdfff8dc021235b0bd49a5
Lalkrishnasubedi <lalkrishnasubedi04@gmail.com>
2026-01-12 21:04:05 +0545

    Initialized

---
+++ app/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from router import router
+from app.router import router
+from app.database import Base, engine
+from database import Base, engine
+from fastapi import FastAPI
+from .router import router
+
+
+import models
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+    "http://127.0.0.1",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(router)
+
+Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    
